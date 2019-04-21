b2791101c8ae55bec1b2f0b3ee05a2839f4beb04
Gasbebe <foo897@naver.com>
2019-04-22 02:46:19 +0900

    add moving

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -7,10 +7,53 @@ public class GamePiece : MonoBehaviour
     public int xIndex;
     public int yIndex;
 
+    bool m_isMoving = false;
+    private void Update()
+    {
+
+        if (Input.anyKeyDown)
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        }
+    }
+
     public void SetCoord(int x, int y)
     {
         xIndex = x;
         yIndex = y;
+    }
+    public void Move(int destX, int destY, float timeToMove)
+    {
+        StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestination = false;
+        float elapsedTime = 0f;
+        m_isMoving = true;
+        while (!reachedDestination)
+        {
+            if(Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+            }
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0, 1f);
+
+            //t = 1 - Mathf.Cos(t * Mathf.PI * 0.5f);
+            //t = t * t;
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            yield return null;
+        }
+        m_isMoving = false;
     }
 
 }
